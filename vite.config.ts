63caab2feea48da264e3975eb6cd87efe0c64bee
joao.rodrigues <joao.rodrigues@dotesfera.com>
2023-05-06 11:10:58 +0100

    pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/vite-react-tailwind-bionic-reading/",
-  plugins: [react()],
-});
